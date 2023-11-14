815f498705119e2e35105f08e60fa6a8969beeef
Jeeva4000 <vjeeva@gmail.com>
2023-11-14 13:05:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Login from "./Pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./Pages/Register";
+import Dashboard from "./Pages/Dashboard";
+import BookTicket from "./Pages/BookTicket";
+import Ticket from "./Pages/Ticket";
+import ViewAllTicket from "./Pages/ViewAllTicket";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard/:userid" element={<Dashboard />} />
+          <Route
+            path="/book-ticket/:userid/:airline"
+            element={<BookTicket />}
+          />
+          <Route path="/ticket/:userid/:ticketid" element={<Ticket />} />
+          <Route path="/allticket/:userid/" element={<ViewAllTicket />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const config = {
+    api: 'https://flight-m11g.onrender.com'
+}
